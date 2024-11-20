2bddb599c025524eb291d43afd6d8a1d53c1b5c1
MichaelPKamm <michael.kamm.5@gmail.com>
2024-11-20 13:35:55 +0100

    Erstellung
    20.11.2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-//import "./App.css";
 import Typography from "@mui/material/Typography";
+import { VegaLite } from "react-vega";
+import chart from "./data/chart.json";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Typography variant="h2">Hello React + MUI!</Typography>
+    <div className="App">
+      <Typography variant="h4">
+        Dies ist eine Grafik der Wetterdaten der Schweiz von 2023
+      </Typography>
+      <VegaLite spec={chart} />
     </div>
   );
 }
