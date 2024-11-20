013b222b4214c1f636bc0ffbad88383081d5b11c
MichaelPKamm <162428014+MichaelPKamm@users.noreply.github.com>
2024-11-20 12:56:02 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//import "./App.css";
+import Typography from "@mui/material/Typography";
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h2">Hello React + MUI!</Typography>
+    </div>
+  );
+}
+
+export default App;
